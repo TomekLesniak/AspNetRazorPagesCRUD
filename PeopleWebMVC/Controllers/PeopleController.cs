b302703cafa3843032cdcf590e41db8f265c3022
TomekLesniak <tomasz.lesniak152@gmail.com>
2021-01-26 20:06:45 +0100

    MVC

---
+++ PeopleWebMVC/Controllers/PeopleController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary.Data;
+using DataLibrary.Models;
+using PeopleWebMVC.Models;
+
+namespace PeopleWebMVC.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private readonly IPeopleData _data;
+
+        public PeopleController(IPeopleData data)
+        {
+            _data = data;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var people = await _data.GetPeople();
+            return View(people);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var data = await _data.GetPersonById(id);
+            var person = new UpdatePersonModel
+            {
+                Id = id,
+                FirstName = data.FirstName
+            };
+            return View(person);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UpdatePersonModel person)
+        {
+            await _data.UpdatePerson(person.Id, person.FirstName);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Create()
+        {
+            var person = new NewPersonModel();
+            return View(person);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewPersonModel person)
+        {
+            var fullPersonModel = new PersonModel()
+            {
+                FirstName = person.FirstName,
+                LastName = person.LastName,
+                DateOfBirth = person.DateOfBirth,
+                IsActive = person.IsActive
+            };
+            int id = await _data.CreatePerson(fullPersonModel);
+            return RedirectToAction("Edit", new {id});
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var person = await _data.GetPersonById(id);
+            return View(person);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(PersonModel person)
+        {
+            await _data.DeletePerson(person.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PeopleWebMVC/Models/NewPersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleWebMVC.Models
+{
+    public class NewPersonModel
+    {
+        [Required] public string FirstName { get; set; }
+        [Required] public string LastName { get; set; }
+        [Required] public DateTime DateOfBirth { get; set; }
+        [Required] public bool IsActive { get; set; }
+    }
+}
